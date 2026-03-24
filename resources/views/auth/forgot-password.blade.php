ecbbe32c910471a41e9f82c273fdb7071eecdf08
mvnz <m.munawar.shaleh@gmail.com>
2026-03-24 19:57:04 +0700

    fix: tambah cache busting pada link auth.css

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -6,7 +6,7 @@
     <title>Lupa Password - NusaMart</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}?v={{ filemtime(public_path('css/auth.css')) }}">
 
 </head>
 <body>
